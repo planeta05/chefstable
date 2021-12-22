068cda30126d337de49535f5f4381a3aab134627
Martin Planeta <30479033+planeta05@users.noreply.github.com>
2021-12-22 20:04:23 +0100

    first styling attempts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import "./styles.css";
 import { useState } from "react";
+import { Flex, Grid, Text, Heading } from "@chakra-ui/react";
 
 export default function App() {
-  const [name, setName] = useState("Martin");
-
-  const handleInputChange = ({ target }) => {
-    setName(target.value);
-  };
+  const list = [
+    {
+      id: 12345,
+      title: "meat balls",
+      category: "meat"
+    },
+    {
+      id: 13345,
+      title: "Bolognese",
+      category: "meat"
+    },
+    {
+      id: 12335,
+      title: "Sahne Torte",
+      category: "meat"
+    }
+  ];
 
   return (
-    <div className="App">
-      <input onChange={(event) => handleInputChange(event)} />
-      <h1>Hello {name}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Grid gridTemplateColumns="2fr 6fr" gridTemplateRows="100vh">
+      <Flex backgroundColor="gray" flexDir="column" height="100vh">
+        <Heading m="10">Chefs Table</Heading>
+        <ul className="nav-list">
+          <li>
+            <Text>Recipes</Text>
+          </li>
+          <li>Calculation</li>
+          <li>Shopping List</li>
+        </ul>
+      </Flex>
+      <div className="main">
+        {list.map((item) => {
+          return <div>{item.title}</div>;
+        })}
+      </div>
+    </Grid>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
+  <ChakraProvider>
     <App />
-  </StrictMode>,
+  </ChakraProvider>,
   rootElement
 );
